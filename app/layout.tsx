9fff5bbc8ddf3c8e2e90826d3ef32f1fdca5738c
Raúl Montejo <raul@clevelandcoder.com>
2024-04-25 11:00:35 -0400

    Use correct propName

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 
       <head>
         <meta charSet="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta name="google-site-verification" content="L2ly-E-6uEJZofo1sBfqD-ul45ZZvvlcjYISh2Ckf5c" />
         <title>Ra&#xFA;l Montejo | Web Developer</title>
