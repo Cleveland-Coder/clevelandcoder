0bde19f11518078b586b0f379cf79b1f001ba42f
Raúl Montejo <raul@clevelandcoder.com>
2024-05-05 21:50:13 -0400

    Swap page conent with résumé header

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,29 @@
+import yaml from 'js-yaml';
+import fs from 'fs';
+
+import { Résumé } from '../types/résumé.interface';
+
+let résumé = {} as Résumé;
+
+try {
+  résumé = yaml.load(fs.readFileSync('./data/résumé.yml', 'utf8')) as Résumé;
+  console.log(résumé);
+} catch (e) {
+  console.log(e);
+}
+
 export default function Home() {
+  const { email, name, occupation } = résumé;
   return (
-    <main>
-      <h1>Ra&#250;l Montejo</h1>
-      <h2>Web Developer</h2>
-      <code>
-        &#60;<span className="tag">portfolio</span>{' '}
-        <span className="attribute">data-upload-time</span>&#61;&#34;
-        <span className="value">right-about-now</span>&#34;&#62;
-      </code>
-    </main>
+    <>
+      <header className="flex flex-col mt-12 text-center">
+        <h1 className="text-4xl sm:text-5xl font-bold">{name}</h1>
+        <h2 className="mt-4 text-xl">{occupation}</h2>
+        <div className="mt-2">
+          <a className="focus:underline hover:underline" href={`mailto:${email}`}>{email}</a>
+        </div>
+      </header>
+      <main></main>
+    </>
   );
 }
