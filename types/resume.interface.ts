966c8980277a17f61386319034042f9665d46fd8
Raúl Montejo <raul@clevelandcoder.com>
2024-05-06 13:45:37 -0400

    Define résumé data types with more detail

--- types/resume.interface.ts
+++ types/resume.interface.ts
@@ -4,20 +4,33 @@ export interface Resume {
   email: string;
   sections: [
     {
+      id: 'experience';
       title: 'Experience';
       jobs: Job[];
     },
     {
+      id: 'education';
       title: 'Education';
-      jobs: Job[];
+      schools: School[];
     },
   ];
 }
 
 export interface Job {
-  job_title: string;
-  company: string;
-  location: string;
-  date: string;
+  id: string;
+  company_name: string;
+  description: string;
   experiences: string[];
+  job_title: string;
+  location: string;
+  period: string;
+}
+
+export interface School {
+  id: string;
+  degree: string;
+  experiences: string[];
+  grad_date: string;
+  institution: string;
+  location: string;
 }
