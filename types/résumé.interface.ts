14bedf1ce190909c9f889197fbb7c3f7b66dfff4
Raúl Montejo <raul@clevelandcoder.com>
2024-05-05 21:49:01 -0400

    Fine-tune shape of résumé data

--- types/résumé.interface.ts
+++ types/résumé.interface.ts
@@ -1,21 +1,23 @@
 export interface Résumé {
-    name: string;
-    title: string;
-    address: string;
-    phone: string;
-    email: string;
-    sections: Section[];
+  name: string;
+  occupation: string;
+  email: string;
+  sections: [
+    {
+      title: 'Experience';
+      jobs: Job[];
+    },
+    {
+      title: 'Education';
+      jobs: Job[];
+    },
+  ];
 }
 
-export interface Section {
-    title: string;
-    items: Item[];
-}
-
-export interface Item {
-    job_title: string;
-    company: string;
-    location: string;
-    date: string;
-    content: string[];
+export interface Job {
+  job_title: string;
+  company: string;
+  location: string;
+  date: string;
+  experiences: string[];
 }
